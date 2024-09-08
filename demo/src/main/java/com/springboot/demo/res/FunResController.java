b0df954da28515bfa7ad90974573acee14bacdfc
shashank725 <shashank.nov725@gmail.com>
2024-09-08 06:18:59 +0530

    [ADD] Custom app properties

--- demo/src/main/java/com/springboot/demo/res/FunResController.java
+++ demo/src/main/java/com/springboot/demo/res/FunResController.java
@@ -1,5 +1,6 @@
 package com.springboot.demo.res;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,16 @@ public class FunResController {
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
+    }
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamname;
+
+    @GetMapping("/teamInfo")
+    public String getTeamInfo() {
+        return "Coach: "+coachName+"   Team Name "+teamname;
     }
 }
